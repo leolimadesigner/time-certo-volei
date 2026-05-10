ab3c053586d9a29e4c102fc0a93c0b3e2f4e78fd
leolimadesigner <leogranito1995@gmail.com>
2026-05-10 15:49:03 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+  export const firebaseConfig = {
+    apiKey: "AIzaSyA6wrrB_fVXGtWzmcwZFmzOGULUnwKkwUc",
+    authDomain: "timecerto-c9d1f.firebaseapp.com",
+    projectId: "timecerto-c9d1f",
+    storageBucket: "timecerto-c9d1f.firebasestorage.app",
+    messagingSenderId: "513517127257",
+    appId: "1:513517127257:web:f5d9632727ef55446c9348",
+    measurementId: "G-H1H0BSQBNR"
+  };
